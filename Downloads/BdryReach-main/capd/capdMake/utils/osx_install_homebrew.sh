5a228b177df2e874fef43308b9ab77a7a6f5bf9b
kalogyu <3200725049@qq.com>
2024-04-02 16:12:20 +0800

    upload

---
+++ Downloads/BdryReach-main/capd/capdMake/utils/osx_install_homebrew.sh
@@ -0,0 +1,3 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# echo export PATH="/usr/local/bin:$PATH" >> ~/.bash_profile
+# export PATH="/usr/local/bin:$PATH"
