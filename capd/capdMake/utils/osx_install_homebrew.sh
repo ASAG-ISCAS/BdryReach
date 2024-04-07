45b59f2f8c06384e5efbf710998574e9ddf493e8
kalogyu <65695455+kalogyu@users.noreply.github.com>
2024-04-07 09:48:45 +0800

    Delete capd directory

--- capd/capdMake/utils/osx_install_homebrew.sh
+++
@@ -1,3 +0,0 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# echo export PATH="/usr/local/bin:$PATH" >> ~/.bash_profile
-# export PATH="/usr/local/bin:$PATH"
